5ddb6ed33c69e6860aa3adbee60378896837e860
egavilan1987 <enmanuelgc@gmail.com>
2018-05-06 13:40:54 -0600

    Table update
    Here, I added new funtionalities to the albums.

--- index.php
+++ index.php
@@ -41,44 +41,37 @@
     </nav>
   </br></br>
     <div class="page-header">
-        <h1>Albums Name</h1>
+        <h1 align="center">Albums</h1>
     </div>
 
-  
+    <!-- Albums -->
+    <div class="container">
+      <div class="row">
 
-
-      <div class="col-md-2">
-        <div class="thumbnail" class="align-items-center">
-          <a href="http://gallery.smartadserver.com/demo_web/image/image_placeholder.png">
-            <img src="http://gallery.smartadserver.com/demo_web/image/image_placeholder.png" alt="Lights" style="width:50%">
-            <div class="caption">
-            <p>Nature</p>
+          <div class="col-lg-3 col-sm-4 col-xs-6">
+            <a title="Image 2" href="#">
+              <img class="thumbnail img-responsive" src="//placehold.it/600x350/2255EE">
+                <p>Nature</p>
+            </a>
+            <div class="align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#viewModal">Details</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#editModal">Edit</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                </div>
+              </div>
           </div>
-          </a>
-          <div class="align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
-            </div>
-          </div>
-        </div>
+          
       </div>
-
-  
-
-    
-
-    
+    </div>
 
 
-
-
-      <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+    <!-- Add album modal -->
+    <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Create New Album</h5>
+            <h4 class="modal-title" id="exampleModalLabel">Create new album</h4>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -86,24 +79,25 @@
           <div class="modal-body">
             <form>
               <div class="form-group">
-                <label for="recipient-name" class="col-form-label">Name:</label>
+                <label for="recipient-name" class="col-form-label">Album title:</label>
                 <input type="text" class="form-control" id="recipient-name">
               </div>
                 <div>
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Add</button>
-          </div>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Add</button>
+                </div>
             </form>
           </div>
         </div>
-        </div>
+     </div>
+    </div>
 
-
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+        <!-- Edit album modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Create New Album</h5>
+            <h4 class="modal-title" id="exampleModalLabel">Edit album</h4>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -111,24 +105,46 @@
           <div class="modal-body">
             <form>
               <div class="form-group">
-                <label for="recipient-name" class="col-form-label">Name:</label>
+                <label for="recipient-name" class="col-form-label">Edit title:</label>
                 <input type="text" class="form-control" id="recipient-name">
               </div>
                 <div>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Add</button>
-          </div>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Save</button>
+                </div>
             </form>
           </div>
         </div>
+     </div>
+    </div>
+
+    <!-- View album details modal -->
+    <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="exampleModalLabel">Album Details</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="recipient-name" class="col-form-label">Title: Title example</label>
+                <br>
+                <label for="recipient-name" class="col-form-label">Total images: 23</label>
+              </div>
+                <div>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </form>
+          </div>
         </div>
+     </div>
+    </div>
+
 
 
   </body>
-
-
-  <div class="container">
-    <div id="tabla"></div>
-  </div>
 </body>
 </html>
